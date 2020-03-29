5d3043fa87e8fc992e71b1f5808cc4853df79602
lucas-brilhante <lucasbrilhantepires@gmail.comgit>
2020-03-29 00:50:14 -0300

    Ajustes no backend.

--- backend/src/controllers/ong_login_controller.js
+++ backend/src/controllers/ong_login_controller.js
@@ -4,10 +4,15 @@ module.exports = {
     async index(req, res) {
         const { ong_id } = req.params;
 
+        const ong_name = await connection('ongs')
+        .where('id',ong_id)
+        .select('name')
+        .first();
+
         const incidents = await connection('incidents')
             .where('ong_id', ong_id)
             .select('*');
 
-        return res.send(incidents);
+        return res.send({...ong_name, incidents});
     },
 }
--- backend/src/controllers/ongs_controller.js
+++ backend/src/controllers/ongs_controller.js
@@ -18,7 +18,7 @@ module.exports = {
             whatsapp,
             city,
             uf
-        }).first();
+        });
 
         return res.send(ong);
     },
